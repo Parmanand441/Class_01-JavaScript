bd80693a1bc2fa54e855887db0135fbd090b666e
Parmanand Rai <106175480+Parmanand441@users.noreply.github.com>
2024-03-05 17:44:27 +0500

    Add files via upload
    Primitive Data types and Non primitive Data types in JavaScript.

---
+++ one.js
@@ -0,0 +1,25 @@
+//nn ss u bb  Primitive Data types in JavaScript./// two categories of data types.. 1 primitive data type and non primitive data types..
+// There are 7 primitive data types in JavaScript...
+// null, number, string, symbol, bigInt, bolean, undefined............ 
+let a=null;
+let b= 255;
+let c= "%";
+let d= "Parmanand_Rai";
+let e= undefined;
+let f=BigInt("70")+BigInt("70");
+let g=true;
+
+console.log(typeof a, typeof b, typeof c) // We use typeOf two find out the data types..
+
+// And there is one non primitive data type in JavaScript//
+/// Which is object...
+
+// Object in js
+
+const item=
+{
+    "Parmanand Rai": 500,
+    "Mayaram":       true,
+    Dhanesh:       "Teacher"
+}
+console.log(item["Parmanand Rai"], item["kanwal"])
---
+++ practice.js
@@ -0,0 +1,12 @@
+// Ch#one practice set.....
+
+let Parmanand=500;
+console.log(typeof Parmanand)
+
+///Qno#02
+
+const item = {
+    Appreciate ="recognize the full worth of",
+    apologize = "mazrat"
+}
+console.log(item['Appreciate'])
